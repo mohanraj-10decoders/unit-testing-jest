d12b74f2281eae31cf670f4e6d59237b30814425
mohanraj-10decoders <mohanraj.l@10decoders.in>
2022-07-21 12:01:41 +0530

    Tests for button & input field

--- src/Counter.js
+++ src/Counter.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
+import './App.css';
 
 const Counter = () => {
   const [counter, setCounter] = useState(0);
+  const [email, setEmail] = useState('');
 
   const incrementCounter = () => {
     setCounter((prevCounter) => prevCounter + 1);
@@ -16,10 +18,23 @@ const Counter = () => {
       <button data-testid='increment' onClick={incrementCounter}>
         +INC
       </button>
+      {/* <font size='7'>TechOnTheNet</font> */}
       <p data-testid='counter'>{counter}</p>
       <button data-testid='decrement' onClick={decrementCounter}>
         -DEC
       </button>
+      <input
+        type='email'
+        placeholder='Enter email'
+        data-testid='email-input'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      {email && !/\S+@\S+\.\S+/.test(email) && (
+        <span className='error' data-testid='error-msg'>
+          Please enter a valid email.
+        </span>
+      )}
     </>
   );
 };
--- src/Counter.test.js
+++ src/Counter.test.js
@@ -1,18 +1,64 @@
 import { render, fireEvent, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Counter from './Counter';
 
-//test block
-test('increments counter', () => {
-  // render the component on virtual dom
-  render(<Counter />);
+describe('<Counter />', () => {
+  test('increments counter', () => {
+    // render the component on virtual dom
+    render(<Counter />);
 
-  //select the elements you want to interact with
-  const counter = screen.getByTestId('counter');
-  const incrementBtn = screen.getByTestId('increment');
+    //select the elements you want to interact with
+    const counter = screen.getByTestId('counter');
+    const incrementBtn = screen.getByTestId('increment');
 
-  //interact with those elements
-  fireEvent.click(incrementBtn);
+    // interact with those elements
+    fireEvent.click(incrementBtn);
 
-  //assert the expected result
-  expect(counter).toHaveTextContent('1');
+    // //assert the expected result
+    expect(counter).toHaveTextContent('1');
+  });
+
+  test('decrements counter', () => {
+    render(<Counter />);
+
+    const counter = screen.getByTestId('counter');
+    const decrementBtn = screen.getByTestId('decrement');
+
+    // fireEvent.click(decrementBtn);
+
+    //userEvent.click => will trigger hover events before clicking. To disable this, set the skipHover option to true.
+    userEvent.click(decrementBtn, undefined, { skipHover: true });
+    expect(counter).toHaveTextContent('-1');
+  });
+
+  test('render email input', () => {
+    render(<Counter />);
+
+    const inputEl = screen.getByTestId('email-input');
+    expect(inputEl).toBeInTheDocument();
+    expect(inputEl).toHaveAttribute('type', 'email');
+  });
+
+  test('pass valid email to test email input field', () => {
+    render(<Counter />);
+
+    const inputEl = screen.getByTestId('email-input');
+    userEvent.type(inputEl, 'test@mail.com');
+
+    expect(screen.getByTestId('email-input')).toHaveValue('test@mail.com');
+    expect(screen.queryByTestId('error-msg')).not.toBeInTheDocument();
+  });
+
+  test('pass invalid email to test input value', () => {
+    render(<Counter />);
+
+    const inputEl = screen.getByTestId('email-input');
+    userEvent.type(inputEl, 'test');
+
+    expect(screen.getByTestId('email-input')).toHaveValue('test');
+    expect(screen.getByTestId('error-msg')).toBeInTheDocument();
+    expect(screen.queryByTestId('error-msg').textContent).toEqual(
+      'Please enter a valid email.'
+    );
+  });
 });
